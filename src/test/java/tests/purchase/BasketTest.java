ee333f479a10aabb849616a6dae9723aba3f536d
Şamil Çevik <shigoof@gmail.com>
2020-06-18 20:52:38 +0300

    Add files via upload

--- src/test/java/tests/purchase/BasketTest.java
+++ src/test/java/tests/purchase/BasketTest.java
@@ -17,7 +17,6 @@ public class BasketTest extends AbstractTest
     @Test
     public void testBasketItem()
     {
-        //login(context.getInternalProps().getUsername(), context.getInternalProps().getPassword());
         //clearBasket();
         MainPage mainPage = new MainPage(browser);
         CategoryPage categoryPage = new CategoryPage(browser);
@@ -44,32 +43,13 @@ public class BasketTest extends AbstractTest
         
         browser.waitAndClick(categoryPage.addBasket);
         Assert.assertEquals(1,1);
-        /*
-         * 
-         * try {
-        	System.in.read();
+        /* 
+        try {
+        	System.in.read(); //Console Wait
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-       
-        browser.waitAndClick(mainPage.shoppingBasketButton);
-
-        while (browser.isElementDisplayed(mainPage.progressBarText))
-        {
-            browser.waitAndClick(mainPage.plusButton);
-        }
-
-        browser.waitAndClick(mainPage.goToBasketButton);
-
-        BasketPage basketPage = new BasketPage(browser);
-        browser.waitAndClick(basketPage.purchaseNote);
-        browser.waitAndSendKeys(basketPage.inputNote, "120 gramlık paketler şeklinde hazırlanmasını istiyorum");
-
-        String basketTotal = basketPage.basketTotal.getText();
-        browser.waitAndClick(basketPage.approveBasket);
-
-        String summaryTotal = basketPage.basketTotal.getText();
-        Assert.assertEquals(basketTotal, summaryTotal);*/
+       */
     }
 }
